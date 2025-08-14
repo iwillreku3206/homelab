507d5a2e96ac2f38a02c70a5f9a23e6d729a2815
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-08-14 18:48:26 +0800

    change stuff

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-gpg -o .env --decrypt --cipher-algo AES256 env
+GPG_TTY=$(tty) gpg -o .env --decrypt --cipher-algo AES256 env
 
 ./00-networks.sh
-docker compose -f 02-pihole.yml up
-docker compose -f 02-pihole.yml up
+docker compose -f 01-pihole.yml up
+docker compose -f 02-cloudflared.yml up
 
 rm .env
