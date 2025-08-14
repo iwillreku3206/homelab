83a60b837aedf038a06ef6f37d4575c6b8112132
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-08-14 18:46:44 +0800

    add stuff

---
+++ 00-networks.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# dns network
+docker network inspect dns > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+  docker network create \
+    --driver bridge \
+    --subnet 172.32.1.252/30 \
+    --gateway 172.32.1.253 \
+    --scope local \
+    dns
+done
+
+# apps network
+docker network inspect apps > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+  docker network create \
+    --driver overlay \
+    --subnet 172.128.0.0/16 \
+    --gateway 172.128.0.1 \
+    --scope swarm \
+    apps
+done
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+gpg -o .env --decrypt --cipher-algo AES256 env
+
+./00-networks.sh
+docker compose -f 02-pihole.yml up
+docker compose -f 02-pihole.yml up
+
+rm .env
