c9ace14b4b75472dbf4c6a364c102ddbf30fb80c
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-08-14 18:53:50 +0800

    passwd

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-GPG_TTY=$(tty) gpg -o .env --decrypt --cipher-algo AES256 env
+
+read -s -p "Enter ENV Password: " GPG_PASS
+
+gpg --passphrase "${$GPG_PASS}" -o .env --decrypt --cipher-algo AES256 env
 
 ./00-networks.sh
 docker compose -f 01-pihole.yml up
