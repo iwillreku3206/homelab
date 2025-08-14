32a21a4d3fda2b27ebc071d08f08b2976c07c568
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-08-15 04:22:29 +0800

    a

--- active-directory.sh
+++ active-directory.sh
@@ -2,7 +2,7 @@
 
 read -s -p "Enter AD Admin Password: " AD_ADMIN_PASSWORD
 
-if [[ -e /etc/samba/smb.conf ]]; do
+if [[ -e /etc/samba/smb.conf ]]; then
   rm /etc/samba/smb.conf
 fi
 samba-tool domain provision --use-rfc2307 --realm HOME2.RINALDOLEE.COM --domain HOME2 --server-role dc --dns-backend SAMBA_INTERNAL --admin-pass "$AD_ADMIN_PASSWORD" 
