81a7ad6656d957d974d942d6ca6262c4aae8b963
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-08-18 20:28:56 +0800

    l

--- sql/setup_postgres.sh
+++ sql/setup_postgres.sh
@@ -22,26 +22,26 @@ if [ -z "${POSTGRES_ROOT_PASSWORD}" ]; then
   rm .env
 fi
 
-psql -h $CORE_INTERNAL_IP -p 5432 -U postgres -W < /tmp/postgres.fifo
+psql -h $CORE_INTERNAL_IP -p 5432 -U postgres -W < /tmp/postgres.fifo &
 
-echo $POSTGRES_ROOT_PASSWORD >> /tmp/postgres.fifo
+echo $POSTGRES_ROOT_PASSWORD > /tmp/postgres.fifo
 
 # Create the database
-echo CREATE DATABASE authentik\; >> /tmp/postgres.fifo
+echo CREATE DATABASE authentik\; > /tmp/postgres.fifo
 
 # Create the user with a password (replace 'yourpassword'!)
-echo CREATE USER authentik WITH PASSWORD \'$AUTHENTIK_PG_PASS\'\; >> /tmp/postgres.fifo
+echo CREATE USER authentik WITH PASSWORD \'$AUTHENTIK_PG_PASS\'\; > /tmp/postgres.fifo
 
 # Give the user full privileges on the database
-echo GRANT ALL PRIVILEGES ON DATABASE authentik TO authentik\; >> /tmp/postgres.fifo
+echo GRANT ALL PRIVILEGES ON DATABASE authentik TO authentik\; > /tmp/postgres.fifo
 
 # Grant schema privilages
 
 echo \c authentik >> /tmp/postgres.fifo
-echo GRANT ALL ON SCHEMA public TO authentik\; >> /tmp/postgres.fifo
-echo ALTER DEFAULT PRIVILEGES IN SCHEMA public >> /tmp/postgres.fifo
-echo     GRANT ALL ON TABLES TO authentik\; >> /tmp/postgres.fifo
-echo ALTER DEFAULT PRIVILEGES IN SCHEMA public >> /tmp/postgres.fifo
-echo     GRANT ALL ON SEQUENCES TO authentik\; >> /tmp/postgres.fifo
+echo GRANT ALL ON SCHEMA public TO authentik\; > /tmp/postgres.fifo
+echo ALTER DEFAULT PRIVILEGES IN SCHEMA public > /tmp/postgres.fifo
+echo     GRANT ALL ON TABLES TO authentik\; > /tmp/postgres.fifo
+echo ALTER DEFAULT PRIVILEGES IN SCHEMA public > /tmp/postgres.fifo
+echo     GRANT ALL ON SEQUENCES TO authentik\; > /tmp/postgres.fifo
 
 rm /tmp/postgres.fifo
