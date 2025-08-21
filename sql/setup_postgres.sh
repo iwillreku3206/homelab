bbbedb608176e3be258b0da66ad319ef2270057d
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-08-22 03:01:26 +0800

    yay

--- sql/setup_postgres.sh
+++ sql/setup_postgres.sh
@@ -29,6 +29,26 @@ PGPASSWORD="$POSTGRES_ROOT_PASSWORD" psql -h $CORE_INTERNAL_IP -p 5432 -U postgr
 create_database() {
   echo CREATE DATABASE IF NOT EXISTS $1\; > /tmp/postgres.fifo
   echo CREATE USER IF NOT EXISTS $1 WITH PASSWORD \'$2\'\; > /tmp/postgres.fifo
+
+  echo DO > /tmp/postgres.fifo
+  echo   '$do$' > /tmp/postgres.fifo
+  echo   BEGIN > /tmp/postgres.fifo
+  echo      'IF EXISTS (' > /tmp/postgres.fifo
+  echo         'SELECT FROM pg_catalog.pg_roles' > /tmp/postgres.fifo
+  echo         "WHERE  rolname = $1) THEN" > /tmp/postgres.fifo
+
+  echo         RAISE NOTICE \'Role \"$1\" already exists. Skipping.\'\; > /tmp/postgres.fifo
+  echo      ELSE > /tmp/postgres.fifo
+  echo         BEGIN   -- nested block > /tmp/postgres.fifo
+  echo            CREATE ROLE $1 LOGIN PASSWORD \'"$2"\'\; > /tmp/postgres.fifo
+  echo         EXCEPTION > /tmp/postgres.fifo
+  echo            WHEN duplicate_object THEN > /tmp/postgres.fifo
+  echo               RAISE NOTICE \'Role \"$1\" was just created by a concurrent transaction. Skipping.\'\; > /tmp/postgres.fifo
+  echo         END\; > /tmp/postgres.fifo
+  echo      END IF\; > /tmp/postgres.fifo
+  echo   END > /tmp/postgres.fifo
+  echo   '$do$;' > /tmp/postgres.fifo
+
   echo GRANT ALL PRIVILEGES ON DATABASE $1 TO $1\; > /tmp/postgres.fifo
   echo \\c $1 >> /tmp/postgres.fifo
   echo GRANT ALL ON SCHEMA public TO $1\; > /tmp/postgres.fifo
