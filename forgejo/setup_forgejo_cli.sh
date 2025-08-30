a60d091662a096b06cb33a0198ff4e7e3f156611
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-08-30 22:42:33 +0800

    forgejo

---
+++ forgejo/setup_forgejo_cli.sh
@@ -0,0 +1,11 @@
+if ! command -v <the_command> >/dev/null 2>&1
+then
+  apt install -y wget apt-transport-https
+  --content-disposition https://code.forgejo.org/forgejo-contrib/-/packages/debian/forgejo-deb-repo/0-0/files/10103
+  apt install -y ./forgejo-deb-repo_0-0_all.deb
+  apt update
+  apt upgrade
+  apt install -y forgejo-bin
+fi
+
+
