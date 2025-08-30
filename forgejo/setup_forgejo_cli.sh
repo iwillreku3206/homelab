0b9d9f24ed9d89b60292c2d3c862e8acbcaf274a
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-08-30 22:43:35 +0800

    oops

--- forgejo/setup_forgejo_cli.sh
+++ forgejo/setup_forgejo_cli.sh
@@ -1,4 +1,4 @@
-if ! command -v <the_command> >/dev/null 2>&1
+if ! command -v forgejo >/dev/null 2>&1
 then
   apt install -y wget apt-transport-https
   --content-disposition https://code.forgejo.org/forgejo-contrib/-/packages/debian/forgejo-deb-repo/0-0/files/10103
