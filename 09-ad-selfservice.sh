3524bfd5aca9cacfbc248f1ad296e385035fddfe
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2025-09-25 19:57:21 +0800

    a

--- 09-ad-selfservice.sh
+++ 09-ad-selfservice.sh
@@ -18,19 +18,27 @@ if [ -z "${BASE_FAST_DIR}" ]; then
   rm .env
 fi
 
-mkdir -p $BASE_FAST_DIR/ad-self-service
+mkdir -p $BASE_FAST_DIR/ad-self-service/config
 
-echo '' > $BASE_FAST_DIR/ad-self-service/config.inc.local.php
+echo '' > $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
 
-echo "<?php"
-echo "	\$keyphrase = \"$AD_SELFSERVE_SECRET\";"
-echo "	\$debug = true;"
-echo "	\$ldap_url = \"ldap://$CORE_INTERNAL_IP\";"
-echo "	#\$ldap_starttls = true;"
-echo "	#putenv(\"LDAPTLS_REQCERT=allow\");"
-echo "	#putenv(\"LDAPTLS_CACERT=/etc/ssl/certs/ca-certificates.crt\");"
-echo "	\$ldap_binddn = \"cn=selfserve,dc=Users,dc=home,dc=rinaldolee,dc=com\";"
-echo "	\$ldap_bindpw = '$AD_SELFSERVE_SECRET';"
-echo "	\$ldap_base = \"dc=home,dc=rinaldolee,dc=com\";"
-echo "	\$ldap_login_attribute = \"uid\";"
-echo "?>"
+echo "<?php" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$keyphrase = \"$AD_SELFSERVE_SECRET\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$debug = true;" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$ldap_url = \"ldap://$CORE_INTERNAL_IP\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$ldap_starttls = true;" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	putenv(\"LDAPTLS_REQCERT=allow\");" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	//putenv(\"LDAPTLS_CACERT=/etc/ssl/certs/ca-certificates.crt\");" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$ldap_binddn = \"cn=selfserve,dc=Users,dc=home,dc=rinaldolee,dc=com\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$ldap_bindpw = '$AD_SELFSERVE_SECRET';" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$ldap_base = \"dc=home,dc=rinaldolee,dc=com\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$ldap_login_attribute = \"uid\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$use_captcha = true;" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$captcha_class = \"InternalCaptcha\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$cache_type = \"Redis\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$cache_redis_url = \"redis.redis:6379\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$cache_namespace = \"adsspCache\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$cache_default_lifetime = 0;" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$ad_mode = true;" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "	\$ldap_filter = \"(&(objectClass=user)(sAMAccountName={login})(!(userAccountControl:1.2.840.113556.1.4.803:=2)))\";" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
+echo "?>" >> $BASE_FAST_DIR/ad-self-service/config/config.inc.local.php
